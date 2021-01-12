a96ea7b571f38e3df7e7540015f1195de59b84a1
Inspirational-sausage-dog <thefleshweak@protonmail.com>
2021-01-12 23:11:25 +0300

    refactoring

--- exceptions.py
+++ exceptions.py
@@ -4,7 +4,3 @@
 class NotCorrectMessageException(Exception):
     """ Message is incorrect and couldn't go throw parsing """
     pass
-
-class CategoryDoesNotExistException(Exception):
-    """ Category does not exist """
-    pass
